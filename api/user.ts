8d576d3b26de93284767ede6f62ebd5ed6368e39
Jarinya008 <jarinyawhan@gmail.com>
2024-03-17 00:39:31 +0700

    z

--- api/user.ts
+++ api/user.ts
@@ -292,18 +292,22 @@ router.get("/score/seven",(req,res) =>{
 
     const username=req.query.username;
     console.log(username);
-const lastSevenDaysDate: Date = new Date();
-lastSevenDaysDate.setDate(lastSevenDaysDate.getDate() - 7);
-
+    const yesterday = new Date();
+    yesterday.setDate(yesterday.getDate() - 7);
+    const yesterdayDay = yesterday.getDate();
+    console.log(yesterdayDay);
+    const yesterdayMonth = yesterday.getMonth() + 1; // เพิ่ม 1 เนื่องจากเดือนเริ่มที่ 0
+    const yesterdayYear = yesterday.getFullYear();
+    const formattedYesterday = `${yesterdayYear}-${yesterdayMonth}-${yesterdayDay}`;
 // const query: string = `
 //              SELECT image.id_image, image.date, image.score_image, image.url_image, image.name_image, vote.day, vote.score_day
 //              FROM vote
 //              INNER JOIN image ON vote.id_image = image.id_image 
 //              WHERE vote.day >= ? AND image.username = ? 
 //              ORDER BY image.id_image, vote.day`;
-const query: string ="SELECT * FROM vote JOIN image ON vote.id_image = image.id_image JOIN user ON image.username = user.username where user.username = ? ORDER BY image.id_image,vote.day";
+const query: string ="SELECT * FROM vote JOIN image ON vote.id_image = image.id_image JOIN user ON image.username = user.username where user.username = ? and vote.day>=? ORDER BY image.id_image,vote.day";
              
-conn.query(query, [username], (err: any, results: any) => {
+conn.query(query, [username,formattedYesterday], (err: any, results: any) => {
     if (err) {
         console.error(err);
         return res.status(500).json({ error: 'Error fetching votes' });
