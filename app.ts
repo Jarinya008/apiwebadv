5a4796c2ee734519d0887d7382765f0ba17a99b8
Jarinya008 <jarinyawhan@gmail.com>
2024-03-28 17:46:08 +0700

    hi

--- app.ts
+++ app.ts
@@ -7,16 +7,10 @@ import http from "http";
 import bodyParser from "body-parser";
 import cors from "cors";
 export const app = express();
+export const router = express.Router();
+import { conn } from "./dbconnect";
 
-const firebaseConfig = {
-  apiKey: "AIzaSyDKHFHiBhRag4Ll8Uv5F9P5zPwJSld6iBI",
-  authDomain: "projectwebadv.firebaseapp.com",
-  projectId: "projectwebadv",
-  storageBucket: "projectwebadv.appspot.com",
-  messagingSenderId: "436147540585",
-  appId: "1:436147540585:web:276b98b409232f187388f8",
-  measurementId: "G-TJCMRLGZ91"
-};
+
 app.use(
     cors({
       origin: "*", //everyone can calling (mistake)
